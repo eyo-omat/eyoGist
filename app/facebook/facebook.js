a210c7f280acae7870de0409b9e63a1c9d61e1e3
Eyioyo <eyo.omatgmail.com>
2017-04-16 14:11:20 +0000

    add facebook js SDK function

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -10,7 +10,19 @@ angular.module('eyoApp.acebook', ['ngRoute', 'ng-facebook'])
 }])
 
 .config( function( $facebookProvider ) {
-  $facebookProvider.setAppId('<your-facebook-app-id>');
+  $facebookProvider.setAppId('126685577873363');
+    $facebookProvider.setPermissions("email","public_profile","user_post","publich_actions", "user_photos");
+})
+
+.run(function($rootScope){
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 })
 
 .controller('facebookController', [function() {
